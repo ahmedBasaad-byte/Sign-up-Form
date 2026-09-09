29993f8026ef60ef35a4be80177a25f74622c796
GHOST_CODE <268465264+ahmedBasaad-byte@users.noreply.github.com>
2026-09-09 20:49:12 +0300

    review for the last time for new

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ const confirmPassword = document.getElementById('confirm-password');
 
 function validatePasswords() {
     if (password.value !== confirmPassword.value) {
-        // If they don't match, add the red border class to BOTH
-        confirmPassword.setCustomValidity("Passwords do not match");
         confirmPassword.style.border = '1px solid red';
         password.style.border = '1px solid red';
-    } else {
-        // If they match, clear the error and reset the borders
+    }
+     else {
         confirmPassword.setCustomValidity("");
         confirmPassword.style.border = '';
         password.style.border = '';
@@ -19,7 +17,7 @@ function validatePasswords() {
 password.addEventListener('input', validatePasswords);
 confirmPassword.addEventListener('input', validatePasswords);
 
-form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', function(event) {
     if (password.value !== confirmPassword.value) {
         event.preventDefault();
         alert("Passwords do not match!");
